55eb92d2f1d9a0c6c5b5e7e85a72dac2fdbcf614
PARK SEONWOONG <sunwoong0169@naver.com>
2024-08-08 21:03:32 +0900

    #5 - 도메인 클래스 설계(게시글 ,댓글)
    도메인 정보를 설계해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록함.
    DB 와의 연동을 위한 추가적인 표현은 추후에 업데이트될 수 있음.

---
+++ fastcampus-project-board/src/main/java/org/example/projectboard/domain/Article.java
@@ -0,0 +1,17 @@
+package org.example.projectboard.domain;
+
+import lombok.extern.java.Log;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title; // 제목
+    private String content; // 내용
+    private String hashtag; // 해시태그
+
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성일시
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
+}
